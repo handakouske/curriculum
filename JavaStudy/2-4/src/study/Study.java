420988299d23060eccb2a6d4ec3d6b0b79298800
handakouske0927 <kouske.0927@gmail.com>
2021-01-20 20:18:29 +0900

    パッケージ

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -1,11 +1,14 @@
 package study;
 
+import constants.FruitsConstants;
 //②Fruits.javaをインポートしなさい。
 import fruits.Fruits;
 /**
  * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
  *
+ *
  * 問①〜問③まであります。
+ *
  * 全て回答し、課題にある画像と同じ表示になるようにしてください。
  *
  */
@@ -17,8 +20,10 @@ public class Study {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
          */
-		Fruits fruits = new Fruits();
-		fruits.printFruits("レモン",20);
+
+
+		Fruits.printFruits((FruitsConstants.FRUITS_LEMON_04),(FruitsConstants.FRUITS_COUNT_20));
+
 
 
 
