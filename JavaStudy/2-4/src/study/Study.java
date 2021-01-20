adefd876a3de30c7106f961ab64f94935c001545
handakouske0927 <kouske.0927@gmail.com>
2021-01-20 20:00:58 +0900

    Revert "パッケージ"
    This reverts commit 45559e08ed442ec400b3962547659d5918a5661c.

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -1,14 +1,11 @@
 package study;
 
-import constants.FruitsConstants;
 //②Fruits.javaをインポートしなさい。
 import fruits.Fruits;
 /**
  * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
  *
- *
  * 問①〜問③まであります。
- *
  * 全て回答し、課題にある画像と同じ表示になるようにしてください。
  *
  */
@@ -20,9 +17,8 @@ public class Study {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
          */
-
-
-		Fruits.printFruits((FruitsConstants.FRUITS_LEMON_04),(FruitsConstants.FRUITS_COUNT_20));
+		Fruits fruits = new Fruits();
+		fruits.printFruits("レモン",20);
 
 
 
